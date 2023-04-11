f056e50c24b7d7107c8d94bded5e9968cfb46c54
CharlesLee <charleslee1120@icloud.com>
2023-04-11 22:53:18 +0800

    Add ubuntu-install-without-docker.sh

---
+++ bash/ubuntu-install-without-docker.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+
+# Copyright VPS Rank Inc.
+
+
+if [ -z "$DOMAIN" ]
+then
+  echo "V2Ray域名未设置,请设置环境变量: DOMAIN"
+  return
+fi
+
+# 生成UUID
+UUID=$(cat /proc/sys/kernel/random/uuid) && echo ${UUID}
+
+# 写入v2ray配置文件
+mkdir -p /opt/vpsrank/docker/compose/v2ray/conf
+
+cat <<EOF > /opt/vpsrank/docker/compose/v2ray/conf/config-first.json
+{
+  "log" : {
+    "access": "/var/log/v2ray/access.log",
+    "error": "/var/log/v2ray/error.log",
+    "loglevel": "warning"
+  },
+  "inbounds": [{
+    "port": 10000,
+    "protocol": "vmess",
+    "settings": {
+      "clients": [
+        {
+          "id": "${UUID}",
+          "level": 1,
+          "alterId": 64
+        }
+      ]
+    },
+    "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+          "path": "/"
+        }
+    },
+    "listen": "0.0.0.0"
+  }],
+  "outbounds": [{
+    "protocol": "freedom",
+    "settings": {}
+  }]
+}
+EOF
+
+cat <<EOF > /opt/vpsrank/docker/compose/v2ray/conf/Caddyfile
+$DOMAIN {
+    reverse_proxy 127.0.0.1:10000
+}
+EOF
+
+cat <<EOF > /opt/vpsrank/docker/compose/v2ray/docker-compose.yaml
+version: '3'
+services:
+  v2ray-instance:
+    container_name: v2ray-instance
+    image: teddysun/v2ray:5.4.0
+    command: /usr/bin/v2ray run -config /etc/v2ray/config-first.json
+    restart: always
+    environment:
+      LANG: en_US.utf8
+      LC_ALL: en_US.utf8
+    ports:
+      - "127.0.0.1:10000:10000"
+    volumes:
+      - "./conf:/etc/v2ray"
+      - "/etc/localtime:/etc/localtime:ro"
+  caddy:
+    container_name: caddy
+    image: caddy:2.6
+    restart: always
+    network_mode: "host"
+    volumes:
+      - "./conf/Caddyfile:/etc/caddy/Caddyfile:ro"
+      - "/etc/localtime:/etc/localtime:ro"
+EOF
+
+# 启动v2ray服务端
+cd /opt/vpsrank/docker/compose/v2ray/ && docker-compose up -d
