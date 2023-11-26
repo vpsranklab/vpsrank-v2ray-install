bfda85d2361e104287ef6404206e37a674827e67
VPSRank <vpsrank.vip@gmail.com>
2023-11-26 23:31:10 +0800

    refactor: Optimizing docker installation steps

--- bash/ubuntu-install-with-docker.sh
+++ bash/ubuntu-install-with-docker.sh
@@ -11,23 +11,8 @@ domain_name=$1
 v2ray_version=$2
 
 # Install Docker
-# Clean installed
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt remove -y $pkg; done
-
-# Add Docker's official GPG key:
 sudo apt update
-sudo apt install -y ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker.io docker-compose
 
 # Generate UUID
 uuid=$(uuidgen)
